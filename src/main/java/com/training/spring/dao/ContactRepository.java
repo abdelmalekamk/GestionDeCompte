ec1e1bca9182dd5a4aaa316419ad1788c4620a95
abdelmalekamk <39907549+abdelmalekamk@users.noreply.github.com>
2018-07-02 21:24:14 +0200

    ajouter des fonction comme ajout supprime ...
    j'ai essayer de terminer mon service en ajoutant les methode ede supression , ajout recherche

--- src/main/java/com/training/spring/dao/ContactRepository.java
+++ src/main/java/com/training/spring/dao/ContactRepository.java
@@ -1,9 +1,16 @@
 package com.training.spring.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.training.spring.entities.Contact;
 
 public interface ContactRepository extends JpaRepository<Contact, Long>{
 
+	@Query("SELECT c FROM contact c where c.com like :x")
+	public Page<Contact> chercher(@Param("x")String mc,Pageable pageable);
+		
 }
--- src/main/java/com/training/spring/web/ContactRestService.java
+++ src/main/java/com/training/spring/web/ContactRestService.java
@@ -4,9 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.training.spring.entities.*;
 import com.training.spring.dao.ContactRepository;
@@ -20,9 +25,34 @@ public class ContactRestService {
 	public List<Contact> getContatcs(){
 		return contactRespository.findAll();
 	}
-	 
-	/*@RequestMapping(value="/contacts/{id}",method=RequestMethod.GET)
-	public Optional<Contact> getContatcs(@PathVariable Long id){
+	  
+	@RequestMapping(value="/chercherContacts",method=RequestMethod.GET)
+	public Page<Contact> chercher(
+			@RequestParam(name="mc" ,defaultValue="") String mc,
+			@RequestParam(name="page" ,defaultValue="0") int page,
+			@RequestParam(name="size" ,defaultValue="5") int size){
+		return null;
+	}
+	
+	@RequestMapping(value="/contacts/{id}",method=RequestMethod.GET)
+	public Optional<Contact> getContatc(@PathVariable Long id){
 		return contactRespository.findById(id);
-	}*/
+	}
+	
+	@RequestMapping(value="/contacts",method=RequestMethod.POST)
+	public Contact saveContact(@RequestBody Contact c){
+		return contactRespository.save(c);
+	}
+	
+	@RequestMapping(value="/contacts/{id}",method=RequestMethod.DELETE)
+	public boolean deleteContact (@PathVariable Long id){
+		 contactRespository.deleteById(id);
+		 return true ;
+	}
+	
+	@RequestMapping(value="/contacts/{id}",method=RequestMethod.PUT)
+	public Contact putContact(@PathVariable Long id,@RequestBody Contact c){
+		c.setId(id);
+		return contactRespository.save(c);
+	}
 }
